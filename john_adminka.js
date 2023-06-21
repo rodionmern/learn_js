0dee3f276f2c61e342ecf95075ae3b8399c550ba
rodiklay <rodeossmith@gmail.com>
2023-06-21 17:32:13 +0300

    2

---
+++ john_adminka.js
@@ -0,0 +1,8 @@
+'use_strict'
+
+let admin;
+let name = "John";
+
+admin=name;
+
+alert(admin)
---
+++ true_names.js
@@ -0,0 +1,6 @@
+/* Find name for
+    let \/, our planet. */
+let planetEarth;
+/* Finв name for
+    let \/, site visitor. */
+let userName;
